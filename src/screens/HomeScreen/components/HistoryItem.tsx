2c9d11f5c0ef853dfe99e3da1719b0f252ca9602
Алексей Остриков <>
2025-11-01 11:51:12 +0200

    refactoring

--- src/screens/HomeScreen/components/HistoryItem.tsx
+++ src/screens/HomeScreen/components/HistoryItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback } from 'react';
+import { FC, memo, useCallback } from 'react';
 import { Block, Colors, ERounding, ESpacings, Row, Typography } from '@UIKit';
 import { HistoryItemType } from '../types.ts';
 import styled from 'styled-components';
@@ -8,7 +8,7 @@ type Props = {
   item: HistoryItemType;
 };
 
-export const HistoryItem: FC<Props> = ({ item }) => {
+export const HistoryItem: FC<Props> = memo(({ item }) => {
   const onPressHandler = useCallback(() => {
     Alert.alert(item.title);
   }, [item.title]);
@@ -35,7 +35,7 @@ export const HistoryItem: FC<Props> = ({ item }) => {
       </Block>
     </Wrapper>
   );
-};
+});
 
 const Wrapper = styled(Pressable).attrs(() => ({
   android_ripple: {
