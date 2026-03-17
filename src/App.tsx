d879a2d2a314b466efd7cad3311beb94273792b8
Aleksey <ostrikov.as@incloud.com>
2026-03-17 15:45:42 +0200

    fix padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigation, navigationRef } from '@navigation';
 import { NavigationContainer } from '@react-navigation/native';
 import I18n from './i18n';
 import { I18nextProvider } from 'react-i18next';
+import { Colors } from '@UIKit';
+import { initialWindowMetrics } from "react-native-safe-area-context";
+import styled from 'styled-components';
+import { Platform } from 'react-native';
 
 const App = () => {
   return (
-    <I18nextProvider i18n={I18n}>
-      <SafeAreaProvider>
+    <StyledSafeAreaProvider>
+      <I18nextProvider i18n={I18n}>
         <NavigationContainer ref={navigationRef}>
           <AppNavigation />
         </NavigationContainer>
-      </SafeAreaProvider>
-    </I18nextProvider>
+      </I18nextProvider>
+    </StyledSafeAreaProvider>
   );
 };
 
 export default App;
+
+const StyledSafeAreaProvider = styled(SafeAreaProvider)({
+  flex: 1,
+  paddingTop: Platform.OS === 'ios' ? 0 : initialWindowMetrics?.insets.top,
+  backgroundColor: Colors.black,
+});
